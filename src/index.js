70f9b37e3a77ecaba8319a0ddcf402429d3208f7
Richard Boardman <rickschmoo@gmail.com>
2018-06-22 23:59:49 +0100

    Posts show stub

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import reducers from './reducers';
 
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+
 
 // this strips out payload from promise returned by axios
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -19,6 +21,7 @@ ReactDOM.render(
       <div>
         <Switch>
             <Route path="/posts/new" component={PostsNew} />
+            <Route path="/posts/:id" component={PostsShow} />
       		  <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
