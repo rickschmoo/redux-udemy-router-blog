2a2c613681435a00b63d83b7abf6555e79e8c147
Richard Boardman <rickschmoo@gmail.com>
2018-06-23 16:46:50 +0100

    Show post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsShow extends Component {
 
+	componentDidMount() {
+		const { id } = this.props.match.params.id;
+		this.props.fetchPosts(id);
+	}
+
 	render() {
+
+		const { post } = this.props;
+
+		if (!post) {
+			return (
+				<div>
+					Loading ...
+				</div>
+			);
+		}
+
 		return(
 			<div>
-				Posts show!
+				<h3>{post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>
 			</div>
 		);
 	}
 }
 
-export default PostsShow;
+function mapStateToProps( { posts }, ownProps ) {
+
+	return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsShow);
